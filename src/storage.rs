a532fb4afd85471bbe7759a1505deecfef094a11
Caleb Maclennan <caleb@alerque.com>
2026-02-12 23:44:02 +0300

    Organize storage modules

---
+++ src/storage.rs
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: © 2026 Caleb Maclennan <caleb@alerque.com>
+// SPDX-License-Identifier: AGPL-3.0-only
+
+#[cfg(feature = "git-annex")]
+pub mod git_annex;
+
+pub mod filesystem;
