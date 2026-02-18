2b6e3c7abb4e46cff0eb57b1a72008dfb65510af
Caleb Maclennan <caleb@alerque.com>
2026-02-18 14:43:10 +0300

    Redo flake using autotools instead of cargo as base build

---
+++ build-aux/pkg.nix
@@ -0,0 +1,108 @@
+# SPDX-FileCopyrightText: © 2026 Caleb Maclennan <caleb@alerque.com>
+# SPDX-License-Identifier: AGPL-3.0-only
+{
+  lib,
+  stdenv,
+  autoreconfHook,
+  gitMinimal,
+  src,
+  version,
+
+  # nativeBuildInputs
+  pkg-config,
+  jq,
+  cargo,
+  rustc,
+  rustPlatform,
+
+  # buildInputs
+  cargo-edit,
+  cargo-deny,
+  git-annex,
+  just,
+  taplo,
+  typos,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "acceptarium";
+  inherit version src;
+
+  # In Nixpkgs, we don't copy the Cargo.lock file from the repo to Nixpkgs'
+  # repo, but we inherit src, and specify a hash (it is a fixed output
+  # derivation). `nix-update` and `nixpkgs-update` should be able to catch that
+  # hash and update it as well when performing updates.
+  cargoDeps = rustPlatform.importCargoLock {
+    lockFile = ../Cargo.lock;
+  };
+
+  nativeBuildInputs = [
+    autoreconfHook
+    cargo
+    git-annex
+    gitMinimal
+    jq
+    pkg-config
+    rustPlatform.cargoSetupHook
+    rustc
+  ];
+
+  preAutoreconf = ''
+    # pretend to be a tarball release so sile --version will not say `vUNKNOWN`,
+    # but don't pretend so hard we make the build system treat us like the tarball.
+    echo ${finalAttrs.version} > .flake-version
+    sed -i -e 's/tarball-version/flake-version/' configure.ac
+  '';
+
+  buildInputs = [
+    gitMinimal
+    git-annex
+  ];
+
+  devShellInputs = [
+    cargo-deny
+    cargo-edit
+    just
+    taplo
+    typos
+  ];
+
+  configureFlags = [ ];
+
+  postPatch = ''
+    patchShebangs build-aux/*.sh build-aux/git-version-gen
+  '';
+
+  strictDeps = true;
+
+  enableParallelBuilding = true;
+
+  # See commentary in bootstrap.sh; we're getting AMINCCLUDE stuff inlined
+  # instead of included but need to avoid a file not found error on first run.
+  postUnpack = ''
+    touch source/aminclude.am
+  '';
+
+  outputs = [
+    "out"
+    "doc"
+    "man"
+    "dev"
+  ];
+
+  meta = {
+    description = "CLI tool to facilitate digitized receipt handling in plain text accounting workflows";
+    longDescription = ''
+      CLI tooling to facilitate scanning receipts, extracting
+      useful data, archiving the assets, and importing the results
+      into Plain Text Accounting systems.
+    '';
+    homepage = "https://codeberg.org/plaintextaccounting/acceptarium";
+    changelog = "https://codeberg.org/plaintextaccounting/acceptarium/src/branch/master/CHANGELOG.md";
+    platforms = lib.platforms.unix;
+    maintainers = with lib.maintainers; [
+      alerque
+    ];
+    license = lib.licenses.agpl3Only;
+  };
+})
--- flake.nix
+++ flake.nix
@@ -2,54 +2,89 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 {
   description = "CLI tool to facilitate digitized receipt handling in plain text accounting workflows";
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    naersk.url = "github:nix-community/naersk";
-    naersk.inputs.nixpkgs.follows = "nixpkgs";
+
+  # To make user overrides of the nixpkgs flake not take effect
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.gitignore = {
+    url = "github:hercules-ci/gitignore.nix";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
+
+  # https://wiki.nixos.org/wiki/Flakes#Using_flakes_with_stable_Nix
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
+
   outputs =
     {
       self,
       nixpkgs,
-      naersk,
+      flake-utils,
+      flake-compat,
+      gitignore,
     }:
-    let
-      cargoToml = (builtins.fromTOML (builtins.readFile ./Cargo.toml));
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-      ];
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-      pkgsFor = forAllSystems (
-        system:
-        import nixpkgs {
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
           inherit system;
-          overlays = [ self.overlays.default ];
-        }
-      );
-    in
-    {
-      overlays.default = final: prev: {
-        "${cargoToml.package.name}" = final.callPackage ./. { inherit naersk; };
-      };
-      packages = forAllSystems (system: {
-        default = pkgsFor.${system}.${cargoToml.package.name};
-        ${cargoToml.package.name} = pkgsFor.${system}.${cargoToml.package.name};
-      });
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = pkgsFor.${system};
-        in
-        {
+        };
+        inherit (gitignore.lib) gitignoreSource;
+        # https://discourse.nixos.org/t/passing-git-commit-hash-and-tag-to-build-with-flakes/11355/2
+        version_rev = if (self ? rev) then (builtins.substring 0 7 self.rev) else "dirty";
+        acceptarium = pkgs.callPackage ./build-aux/pkg.nix {
+          version = "${(pkgs.lib.importTOML ./Cargo.toml).package.version}-${version_rev}-flake";
+          src = pkgs.lib.cleanSourceWith {
+            # Ignore many files that gitignoreSource doesn't ignore, see:
+            # https://github.com/hercules-ci/gitignore.nix/issues/9#issuecomment-635458762
+            filter =
+              path: type:
+              !(builtins.any (r: (builtins.match r (builtins.baseNameOf path)) != null) [
+                # Nix files
+                "flake.nix"
+                "flake.lock"
+                "default.nix"
+                "shell.nix"
+                # git commit and editing format files
+                ".editorconfig"
+                # Git files
+                ".git"
+              ]);
+            src = gitignoreSource ./.;
+          };
+        };
+      in
+      rec {
+        devShells = {
           default = pkgs.mkShell {
-            inputsFrom = [ pkgs.${cargoToml.package.name} ];
-            buildInputs = with pkgs; [
-              libgit2
+            inherit (acceptarium)
+              buildInputs
+              nativeCheckInputs
+              ;
+            nativeBuildInputs = acceptarium.nativeBuildInputs ++ acceptarium.devShellInputs;
+            configureFlags = acceptarium.configureFlags ++ [
+              "--enable-developer-mode"
             ];
           };
-        }
-      );
-    };
+        };
+        packages = {
+          acceptarium = acceptarium;
+          acceptarium-clang = acceptarium.override {
+            # Use the same clang version as Nixpkgs' rust clang stdenv
+            stdenv = pkgs.rustc.llvmPackages.stdenv;
+          };
+        };
+        defaultPackage = packages.acceptarium;
+        apps = rec {
+          default = acceptarium;
+          acceptarium = {
+            type = "app";
+            program = "${self.defaultPackage.${system}}/bin/acceptarium";
+          };
+        };
+        defaultApp = apps.acceptarium;
+      }
+    );
 }
