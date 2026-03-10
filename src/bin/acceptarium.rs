98caf058892fe91f7c9450c0c0520ca39bcdde65
Caleb Maclennan <caleb@alerque.com>
2026-03-10 17:00:18 +0300

    Instanciate logging before we parse the config, which itself can error

--- src/bin/acceptarium.rs
+++ src/bin/acceptarium.rs
@@ -6,23 +6,35 @@ use acceptarium::{Config, Result};
 use acceptarium::{process, run, status, storage};
 
 use clap::{CommandFactory, FromArgMatches};
-use flexi_logger::Logger;
+use flexi_logger::{Logger, LoggerHandle};
+use log::LevelFilter;
 
-fn main() -> Result<()> {
+fn main() {
+    let logger = Logger::with(LevelFilter::Error)
+        .format_for_stderr(flexi_logger::colored_default_format)
+        .log_to_stderr();
+    let logger = logger
+        .start()
+        .unwrap_or_else(|e| panic!("Unable to start logger: {:?}", e));
+    if let Err(e) = run(logger) {
+        log::error!("{:?}", e);
+        std::process::exit(1);
+    }
+}
+
+fn run(logger: LoggerHandle) -> Result<()> {
     let version = option_env!("VERGEN_GIT_DESCRIBE").unwrap_or_else(|| env!("CARGO_PKG_VERSION"));
     let app = Cli::command().version(version).styles(STYLES);
     let matches = app.get_matches();
-    let args = Cli::from_arg_matches(&matches).expect("Unable to parse arguments");
+    log::debug!("Attempting to parse CLI arguments, {:?}", matches);
+    let args = Cli::from_arg_matches(&matches)?;
+    log::debug!("Mixing up runtime config from defaults, config file, env vars, and CLI flags");
+    log::debug!("CLI Args: {:?}", &args);
     let config = Config::new(&args)?;
-    let logger = Logger::with(config.verbosity)
-        .format_for_stderr(flexi_logger::colored_default_format)
-        .log_to_stderr();
-    logger.start()?;
-    log::debug!("Args: {:?}", &args);
-    log::info!("Mapped defaults, config file, env vars, and CLI flags to runtime configuration");
+    logger.set_new_spec(config.verbosity.into());
     log::debug!("Completed config: {:?}", &config);
-    log::info!("Passing subcommand to matched handler");
-    let result = match Commands::from_arg_matches(&matches)? {
+    log::debug!("Passing subcommand to matched handler");
+    match Commands::from_arg_matches(&matches)? {
         Commands::Add { files, .. } => storage::add(&config, files),
         Commands::List { json, .. } => storage::list(&config, json),
         Commands::Process { id, .. } => process::process(&config, &id),
@@ -35,10 +47,5 @@ fn main() -> Result<()> {
             let name = args.pop().ok_or("external command without a name")?;
             run::run(&config, name, args)
         }
-    };
-    if let Err(e) = result {
-        log::error!("{:#}", e);
-        std::process::exit(1);
     }
-    Ok(())
 }
